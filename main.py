ed43c53e86af93c291f788b361c498ef36fd8c16
Kang Hui <kanghuidl@qq.com>
2019-05-02 19:37:44 +0800

    Update traning code

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torchvision as tv
 
 from models import Vgg16
 from models import TransNet
 from dataset import ImageFolder
-from torchvision import transforms
 from torch.utils.data import DataLoader
 
 
@@ -24,7 +23,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data', type=str, default=data)
 parser.add_argument('--epochs', type=int, default=10)
 parser.add_argument('--lr', type=float, default=1e-3)
-parser.add_argument('--batch_size', type=int, default=1)
+parser.add_argument('--batch_size', type=int, default=8)
+parser.add_argument('--style_weight', type=float, default=1e5)
+parser.add_argument('--content_weight', type=float, default=1e0)
 cfg = parser.parse_args()
 print(cfg)
 
@@ -35,8 +36,10 @@ def gram_matrix(x):
     gram = torch.bmm(ft, ft_t) / (c * h * w)
     return gram
 
-dataset = ImageFolder(cfg.data, transform=transforms.ToTensor())
+dataset = ImageFolder(cfg.data, transform=tv.transforms.ToTensor())
 dataloader = DataLoader(dataset, batch_size=cfg.batch_size, shuffle=True)
+
+style, content = iter(dataloader).next()
 
 vgg16 = Vgg16().to(device)
 transform = TransNet().to(device)
@@ -44,7 +47,7 @@ transform = TransNet().to(device)
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(transform.parameters(), lr=cfg.lr)
 
-# plt.ion()
+plt.ion()
 for epoch in range(cfg.epochs):
     print('Epoch: {}/{}'.format(epoch + 1, cfg.epochs))
 
@@ -64,8 +67,10 @@ for epoch in range(cfg.epochs):
             gm_y = gram_matrix(ft_y)
             gm_s = gram_matrix(ft_s)
             style_loss += criterion(gm_y, gm_s)
+        style_loss *= cfg.style_weight
 
         content_loss = criterion(fts_y.relu2_2, fts_x.relu2_2)
+        content_loss *= cfg.content_weight
 
         total_loss = style_loss + content_loss
         total_loss.backward()
@@ -77,13 +82,17 @@ for epoch in range(cfg.epochs):
             'style_loss: {:.4f}, content_loss: {:.4f}, total_loss: {:.4f}'.format(style_loss.item(), content_loss.item(), total_loss.item())
         )
 
-        # if i % 100 == 99:
-        #     fake = net_G(fixed_noises)
-        #     fake = fake.detach().cpu()
-        #     fake = tv.utils.make_grid(fake)
-        #     fake = fake.numpy().transpose(1, 2, 0)
-        #     plt.imshow(fake)
-        #     plt.pause(0.1)
+        content = content.to(device)
+        outputs = transform(content)
 
-# plt.ioff()
-# plt.show()
+        content = content.detach().cpu()
+        outputs = outputs.detach().cpu()
+
+        display = torch.cat((style[:3], content[:3], outputs[:3]), 0)
+        display = tv.utils.make_grid(display, nrow=3)
+        display = display.numpy().transpose(1, 2, 0)
+        plt.imshow(display)
+        plt.pause(0.1)
+
+plt.ioff()
+plt.show()
